18b5317d24b5ee866ea1c5b82693dea5a91648b1
crisbertuh <cristianomartins@alunos.utfpr.edu.br>
2025-09-01 08:24:17 -0300

    Add files via upload

---
+++ Itens_projeto.c
@@ -0,0 +1,21 @@
+/*PROJETO FINAL DA DISCIPLINA
+
+ITENS IMPORTANTES
+--> O porgrama final deve ser rodado e entregue em C
+--> Escolher um método de classificação binária
+--> 80% dos dados devem ser utilizados para treinamento da IA
+--> 20% dos dados devem ser utilizados para teste da IA
+--> O trabalho deve ser feito em dupla.
+
+
+
+
+
+
+
+
+
+
+
+
+*/
