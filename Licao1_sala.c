4aea9153f225b4c09c40280d9d0e76820437892c
crisbertuh <cristianomartins@alunos.utfpr.edu.br>
2025-09-15 08:52:34 -0300

    Create Licao1_sala.c

---
+++ Licao1_sala.c
@@ -0,0 +1,31 @@
+/*
+Faça um programa em linguagem C que possua:
+1. Uma função para alocar dinamicamente uma string com um tamanho definido
+pelo usuário;
+2. Leia a string e a coloque no vetor alocado no item 1;
+3. Uma função que mostre a string na tela;
+4. Uma função de libere o espaço de memória alocado para a string antes de
+encerrar o programa.
+
+Exemplo:
+void main( )
+{
+// string a ser a locada
+char *nome;
+int tamanho;
+// Pedir ao usuário a tamanho da string
+// Função que aloca a memória para a string
+aloca_string(nome, tamanho);
+// colocar a string digitada pelo usuário na variável “nome”
+// mostra_string(nome);
+// libera_memoria(nome);
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+
+}
